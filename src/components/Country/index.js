9bd8b5c133da450b3cb6e58ea54dcdaf58b8fdf5
quangdm1702 <doanminhquang17022003@gmail.com>
2024-08-29 07:50:54 +0700

    Scss trong React

--- src/components/Country/index.js
+++ src/components/Country/index.js
@@ -1,15 +1,16 @@
 import { country } from "../../data/country";
+import './Country.scss';
 
 function Country() {
   return (
     <>
-      <ul>
+      <ul className="country">
         {(country || []).map(itemCountry => (
-          <li key={itemCountry.id}>
-            <span>{itemCountry.name}</span>
-            <ul>
+          <li className="country__item" key={itemCountry.id}>
+            <span className="country__text">{itemCountry.name}</span>
+            <ul className="country__sub">
               {(itemCountry.city || []).map(itemCity => (
-                <li key={itemCity.id}>{itemCity.name}</li>
+                <li className="country__sub--item" key={itemCity.id}>{itemCity.name}</li>
               ))}
             </ul>
           </li>
